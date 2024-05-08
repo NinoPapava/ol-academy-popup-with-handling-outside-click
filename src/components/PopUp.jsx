a3b6dec2aa215d21fe49ff28c60f0ef1e5501899
NinoPapava <gv.papava@gmail.com>
2024-05-08 18:51:56 +0400

    კოდის განახლება

--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -1,40 +1,59 @@
 import { useRef, useState, useEffect } from 'react'
-import { PopupBack } from './PopupBack'
-import { PopupContent } from './PopupContent';
 import '../styles/PopUp.scss'
 
 const PopUp = () => {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpenButton, setIsOpenButton] = useState(false)
+  const [isPopupPage, setIsPopupPage] = useState(false);
   const popupRef = useRef(null);
 
   const handleClickOutside = (event) => {
-    if (popupRef.current && popupRef.current.contains(event.target)) {
-      setIsOpen(false);
+    if (popupRef.current && !popupRef.current.contains(event.target)) {
+      setIsOpenButton(false);
+      setIsPopupPage(false);
     }
   };
 
   useEffect(() => {
-    isOpen
+    isOpenButton
       ? document.addEventListener('mousedown', handleClickOutside)
       : document.removeEventListener('mousedown', handleClickOutside);
 
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
-  }, [isOpen]);
+  }, [isOpenButton]);
+
+  const handleClick = (event) => {
+    event.stopPropagation();
+  }
+  const handleOpen = () => {
+    setIsOpenButton(true);
+    setIsPopupPage(true);
+  };
+
+  const handleClose = () => {
+    setIsOpenButton(false);
+    setIsPopupPage(false);
+  };
 
   return (
     <div className='container'>
-      <div className="container-background">
-        {isOpen ? (
+      <div className="container-background" ref={popupRef} >
+        {isOpenButton && isPopupPage ? (
           <>
-            <PopupBack onClick={() => setIsOpen(!isOpen)}>
-              <PopupContent onClose={() => setIsOpen(!isOpen)} />
-            </PopupBack>
+            <div className='background' onClick={() => setIsPopupPage(false)}>
+              <div className='content' onClick={handleClick} >
+                <h1>10% OFF! DON'T MISS OUT!</h1>
+                <p>9 minutes 54 seconds</p>
+                <div className='close-button'>
+                  <button onClick={handleClose}>Close</button>
+                </div>
+              </div>
+            </div>
           </>
         ) : (
           <>
-            <button onClick={() => setIsOpen(!isOpen)}>{isOpen ? 'Close' : 'ORDER IT'}</button>
+            <button onClick={handleOpen}>{isOpenButton ? 'Close' : 'ORDER IT'}</button>
           </>
         )}
       </div>
